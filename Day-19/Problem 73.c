e6ad968ac51d164ed8c48b57758bda08623134fd
btech-sj-clg-2025 <25116cn041@glbitm.ac.in>
2026-06-28 18:15:16 +0530

    Day-19 uploaded.

---
+++ Day-19/Problem 73.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+    int matrix1[100][100], matrix2[100][100], sum[100][100];
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("\nEnter elements of the first matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of the second matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    printf("\nResultant Matrix (Sum):\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
---
+++ Day-19/Problem 76.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int principal_sum = 0, secondary_sum = 0;
+    int matrix[10][10];
+
+    printf("Enter the size of the square matrix (e.g., 3 for 3x3): ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        principal_sum += matrix[i][i];            
+        secondary_sum += matrix[i][n - i - 1];    
+    }
+
+    printf("\n--- Result ---\n");
+    printf("Sum of Principal Diagonal: %d\n", principal_sum);
+    printf("Sum of Secondary Diagonal: %d\n", secondary_sum);
+
+    return 0;
+}
